041c2a324827a5751b6f9e9780341af24045e843
Tomek <tomek.wojak796@gmail.com>
2025-10-22 23:25:19 +0200

    Created even and odd numbers counter

--- python-training/main.py
+++ python-training/main.py
@@ -4,7 +4,23 @@ z = float(3)
 
 print(x,type(x),y, type(y),z)
 
-numbers = [1,2,3,4,5,6,7,8,9,10]
 
+numbers = [1,2,3,4,5,6,7,8,9,10]
 for i in numbers:
     if(i % 2 == 0): print(f'{i} jest liczbą parzystą')
+
+# even counter
+def evenCounter():
+    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    even_count = 0
+    odd_count = 0
+
+    for num in numbers:
+        if num % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+
+    print(f'Parzyste: {even_count} \nNieparzyste: {odd_count}')
+
+evenCounter()
