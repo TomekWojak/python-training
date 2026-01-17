68234d3a33b9ba6356f3de68dce1740c3b20ec5d
Tomasz Wojak <73196@student.wsiz.edu.pl>
2026-01-17 15:26:56 +0100

    zadania 1-3

--- python-training/script.py
+++ python-training/script.py
@@ -1,10 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-kategorie = ['A', 'B', 'C', 'D']
-udzial = [30, 20, 25, 25]
-plt.pie(udzial, labels=kategorie,
+# Zad 1
+produkty = ['Mleko', 'Chlyb', 'Woda', 'Warzywa', 'Owoce', 'Słodycze']
+iloscSprzedanych = [100, 250, 150, 110,203,303]
+plt.bar(produkty, iloscSprzedanych)
+plt.show()
+
+# Zad 2
+plt.pie(iloscSprzedanych, labels=produkty,
 autopct='%1.f%%', startangle=90,
 colors=['skyblue', 'lightgreen',
 'lightcoral', 'gold'])
 plt.title('Udział w kategoriach')
+plt.show()
+
+# Zad 3
+czas = [1, 2, 3, 4, 5]
+prChwilowa = [2, 4, 6, 8, 10]
+plt.scatter(czas, prChwilowa)
 plt.show()
