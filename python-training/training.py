5ea12ebbc046e279ec291322ab4dcd04a54af51f
Tomek <tomek.wojak796@gmail.com>
2026-01-20 13:09:32 +0100

    finished task 9

--- python-training/training.py
+++ python-training/training.py
@@ -1,18 +1,5 @@
-iloscWczytywanychLiczb = 5
-licznik = 1
-suma = 0
-
-def obliczSrednia(suma):
-    srednia = suma / iloscWczytywanychLiczb
-    print(suma)
-    return srednia
-
-while licznik <= 5:
-    try:
-        x = int(input('Podaj liczbę: '))
-        suma += x
-        licznik += 1
-    except ValueError:
-        print('Podano nieprawidłową wartość')
-
-print(obliczSrednia(suma))
+lista =  ['kot','programowanie','dom']
+dlugosci = []
+for el in lista:
+    dlugosci.append(len(el))
+print(dlugosci)
